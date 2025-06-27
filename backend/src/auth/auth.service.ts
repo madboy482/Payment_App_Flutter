44c80f81445608dfe1126d094da33886d818ccdd
MadBoy <73926361+madboy482@users.noreply.github.com>
2025-06-27 20:37:28 +0530

    add

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.validateUser(username, pass);
+    return user;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user._id, roles: user.roles };
+    return {
+      access_token: this.jwtService.sign(payload),
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        roles: user.roles,
+      },
+    };
+  }
+}
